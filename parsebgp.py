7c10a4c9dbeab3963dbfa93a22cca738aa59e405
nrieb <nrieb@cs.unm.edu>
2013-11-26 22:05:08 -0700

    parsebgp changed to look for nicer bgp files in the good list

--- parsebgp.py
+++ parsebgp.py
@@ -3,6 +3,7 @@
 import dpkt
 import socket
 import sys
+import fileinput
 
 def type_to_string(num):
     if num == 1:
@@ -16,9 +17,11 @@ def type_to_string(num):
     elif num == 5:
         return "ROUTE_REFRESH"
 
-
+bad_bgp = 0
+bgp_type = [0 for x in xrange(0,5)]
 # trying 24b-router.pcap for now... probably a better pcap file
-for file_name in sys.argv[1:]:
+#for file_name in sys.argv[1:]:
+for file_name in fileinput.input():
     with open(file_name, 'rb') as f:
         for ts, pkt in dpkt.pcap.Reader(f):
             
@@ -35,16 +38,21 @@ for file_name in sys.argv[1:]:
             # tcp data shouldn't be blank; dunno why it can be, just dropping them for now
             if (tcp.dport == 179 or tcp.dport == 179) and len(tcp.data):
                 try:
-                    print type(dpkt.bgp.BGP(tcp.data))
+                   # print type(dpkt.bgp.BGP(tcp.data))
                     bgp = dpkt.bgp.BGP(tcp.data)
+                    bgp_type[bgp.type - 1] += 1
+                    '''
                     print type_to_string(bgp.type)
                     if bgp.type == dpkt.bgp.UPDATE:
                         for attr in bgp.update.attributes:
                             if attr.type == dpkt.bgp.AS_PATH:
                                 print path_to_str(attr.as_path)
                                 break
-                        
+                                ''' 
                 except:
                     # misformed bgp packets will cause an exception, i believe
                     # if there are a ton of them, we may have to handle them, but otherwise its ok
-                    continue
+                    bad_bgp += 1
+    print file_name
+    print 'bad', [type_to_string(idx+1) for idx in xrange(0,5)]
+    print bad_bgp,  bgp_type
