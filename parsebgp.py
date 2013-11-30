c33de7d04f4aabddc7b91ee594ce3c5331b08a61
nrieb <nrieb@cs.unm.edu>
2013-11-29 19:00:22 -0700

    Very Basic AS Path parsing working (i.e. it doesn't break).  script assumes 4 octet AS path values; breaks otherwise.  Added modified dpkt to be able to parse 4 octet AS path values.  added pcap files that i used

--- parsebgp.py
+++ parsebgp.py
@@ -1,9 +1,18 @@
 #! /usr/bin/env python
 
-import dpkt
+'''
+usage:
+python parsebgp.py <pcap file>
+Note: this currently assumes that the AS in the AS path
+are 4 octets long NOT 2 octets.  If this is not true, this
+will break
+'''
+
 import socket
 import sys
 import fileinput
+import struct
+import dpkt
 
 def type_to_string(num):
     if num == 1:
@@ -17,14 +26,55 @@ def type_to_string(num):
     elif num == 5:
         return "ROUTE_REFRESH"
 
+def update_parse(bgp_update):
+    assert bgp_update.type == dpkt.bgp.UPDATE
+    for attr in bgp_update.update.attributes:
+        if attr.type == bgp.AS_PATH:
+            print path_to_str(attr.as_path)
+            break
+
+def bgp_parse(raw_tcp_data, packet_num):
+    tcp_len = len(raw_tcp_data)
+    header_len = 16+2+1
+    l = []
+    i = 0
+    while tcp_len > 0:
+        try:
+            data_len = struct.unpack(">H", raw_tcp_data[16:18])[0]
+        except:
+            #print dpkt.hexdump(raw_tcp_data)
+            #print packet_num, "failed on sub-packet" , i, log_len, tcp_len
+            return l
+        try:
+            l.append(dpkt.bgp.BGP(raw_tcp_data[:header_len + data_len]))
+        except dpkt.dpkt.UnpackError:
+            print packet_num, "failed on sub-packet" , i
+            pass
+
+            
+        #print len(raw_tcp_data[:data_len])
+        #print dpkt.hexdump(raw_tcp_data[:header_len])
+        raw_tcp_data = raw_tcp_data[data_len:]
+        tcp_len -= data_len
+        i+=1
+    return l
+
+
+#ip4 4 octet to string
+def ip4_to_str(ip4):
+    return "%d.%d.%d.%d" % struct.unpack("@BBBB", ip4)
+    
+
 bad_bgp = 0
-bgp_type = [0 for x in xrange(0,5)]
-# trying 24b-router.pcap for now... probably a better pcap file
-#for file_name in sys.argv[1:]:
-for file_name in fileinput.input():
+count = 0
+# trying 24b-router.pcap
+for file_name in sys.argv[1:]:
+#for file_name in fileinput.input():
     try:
+        i = 0
         with open(file_name.strip(), 'rb') as f:
             for ts, pkt in dpkt.pcap.Reader(f):
+                i+=1
                 eth=dpkt.ethernet.Ethernet(pkt)
                 
                 if eth.type!=dpkt.ethernet.ETH_TYPE_IP:
@@ -36,29 +86,19 @@ for file_name in fileinput.input():
                 
                 tcp = ip.data
                 
-                # tcp data shouldn't be blank; dunno why it can be, just dropping them for now
-                if type(tcp) != str and len(tcp.data) and (tcp.dport == 179 or tcp.dport == 179):
+                # ignore tcp setup (len == 0)
+                if (tcp.dport == 179 or tcp.sport == 179) and len(tcp.data):
                     try:
-                        # print type(dpkt.bgp.BGP(tcp.data))
-                        bgp = dpkt.bgp.BGP(tcp.data)
-                        bgp_type[bgp.type - 1] += 1
-                        '''
-                        print type_to_string(bgp.type)
-                        if bgp.type == dpkt.bgp.UPDATE:
-                        for attr in bgp.update.attributes:
-                        if attr.type == dpkt.bgp.AS_PATH:
-                        print path_to_str(attr.as_path)
-                        break
-                                ''' 
+                        if i == 54:
+                            data = bgp_parse(tcp.data, i)
+                            print data
+                            print len(data)
+                        else:
+                            bgp_parse(tcp.data, i)
                     except:
-                        # misformed bgp packets will cause an exception, i believe
-                        # if there are a ton of them, we may have to handle them, but otherwise its ok
+                        #print "fail on packet", i, ip4_to_str(ip.src), ip4_to_str(ip.dst)
                         bad_bgp += 1
-        print ''
-        print file_name.strip()
-        print 'bad', [type_to_string(idx+1) for idx in xrange(0,5)]
-        print bad_bgp,  bgp_type
-        bad_bgp = 0
-        bgp_type = [0 for x in xrange(0,5)]
+                        raise
+
     except dpkt.dpkt.NeedData:
         continue
