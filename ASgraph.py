75d8ef79df630a5238879b3d98fb154c9a745948
nrieb <nrieb@cs.unm.edu>
2013-12-01 16:31:21 -0700

    ASgraph should be working (TODO see if i need to propagate withdrawn). ASgraph has every node along the path having withdrawn and announced; could change later. parsebgp now only looks at a particular ip and only at things talking to that ip (the ip i consider most interesting) Added graph to parsebgp and nothing *break* when it runs

--- ASgraph.py
+++ ASgraph.py
@@ -1,29 +1,59 @@
 #! /usr/bin/env python
 
 class ASGraph(object):
-    self.nodes = {}
-    self.graph = []
+    # graph with 1 root node set means we assume only one directed
+    # edge between any two nodes
+
+    def __init__(self, nodes={}, graph=[]):
+        self.nodes = {'root' : self.Node(0, [], [])}
+        self.graph = [set([])]
+        for AS, node in nodes.viewitems():
+            if AS == 'root':
+                continue
+            self[AS] = node
+
+        for adj_list in graph:
+            self.graph.append(adj_list)
+            
     
-    def add(self, path, subnets):
-        prevKey = -1
-        #TODO see if this path needs to be reversed or not
+    def add(self, path, subnets, withdrawn):
+        #TODO see if i need to propagate withdrawn further
+        #if i do, then i have to make sure nothing else pointing to it
+        #with the same subnet
+        assert len(path)
+        #add to root first
+        prevKey = 0
+        #path is initially closest to farthest
         for AS in path:
-            if !self.nodes.has_key(AS):
-                self.nodes[AS] = self.Node(len(self.graph))
+            assert AS != 'root'
+            if not self.nodes.has_key(AS):
+                self.nodes[AS] = self.Node(len(self.graph), [], [])
                 self.graph.append(set([]))
-            
-            if prevKey != -1:
-                self.graph[prevKey].add(self.nodes[AS])
-            
-            prefKey = self.node[AS].key
+
+            self.graph[prevKey].add(self.nodes[AS])
+            self.nodes[AS].add_subnets(subnets)
+            self.nodes[AS].add_withdrawn(withdrawn)
+            prefKey = self.nodes[AS].key
         
         
+
+    def __repr__(self):
+        return "ASGraph(nodes=%r, graph=%r)" % (self.nodes, self.graph)
 
     class Node(object):
         
-        def __init__(self, key):
+        def __init__(self, key, subnets, withdrawn):
             self.key = key
-            self.subnets = []
+            self.subnets = subnets
+            self.withdrawn = withdrawn
 
-        def add_subnets(self, subnets): 
-            self.subnets.append(subnet)
+        def add_subnets(self, subnets):
+            for subnet in subnets:
+                self.subnets.append(subnet)
+        
+        def add_withdrawn(self, subnets):
+            for subnet in subnets:
+                self.withdrawn.append(subnets)
+
+        def __repr__(self):
+            return "self.Node(key=%r, subnets=%r, withdrawn=%r)" % (self.key, self.subnets, self.withdrawn)
--- parsebgp.py
+++ parsebgp.py
@@ -13,6 +13,9 @@ import sys
 import fileinput
 import struct
 import dpkt
+from ASgraph import *
+
+FOUR_OCTET_AS = 65
 
 def type_to_string(num):
     if num == 1:
@@ -59,6 +62,28 @@ def bgp_parse(raw_tcp_data, packet_num):
         i+=1
     return l
 
+def get_AS(bgp_open):
+    AS = bgp_open.asn
+    for param in bgp_open.parameters:
+        if param.type == dpkt.bgp.CAPABILITY and param.capability.code == FOUR_OCTET_AS:
+            AS = struct.unpack(">I", param.capability.data)[0]
+            break
+    return AS
+
+def unpack_segments(segments):
+    return [x.data for x in segments]
+
+def process_update(graph, bgp_update, src_AS):
+    #add in a singleton path for when there is not AS path
+    #attribute.  This is because we can withdraw when there is no
+    #path
+    path = [src_AS]
+    for attribute in bgp_update.attributes:
+        if attribute.type == dpkt.bgp.AS_PATH:
+            [path] = unpack_segments(attribute.as_path.segments)
+            break
+
+    graph.add(path, bgp_update.announced, bgp_update.withdrawn)
 
 #ip4 4 octet to string
 def ip4_to_str(ip4):
@@ -67,13 +92,16 @@ def ip4_to_str(ip4):
 
 bad_bgp = 0
 count = 0
-withdrawn_count = 0
+withdrawn_no_path_count = 0
+# Assuming that pcap started part way through.  these are the AS numbers of the ips directly talking to 109
+ip_list = {'192.168.212.116': 65716, '192.168.212.113': 65713, '192.168.212.112': 65812}
 # trying 24b-router.pcap
 for file_name in sys.argv[1:]:
 #for file_name in fileinput.input():
     try:
         i = 0
         with open(file_name.strip(), 'rb') as f:
+            graph = ASGraph()
             for ts, pkt in dpkt.pcap.Reader(f):
                 i+=1
                 eth=dpkt.ethernet.Ethernet(pkt)
@@ -88,22 +116,24 @@ for file_name in sys.argv[1:]:
                 tcp = ip.data
                 
                 # ignore tcp setup (len == 0)
-                if (tcp.dport == 179 or tcp.sport == 179) and len(tcp.data):
+                # 192.168.212.109 looks like the router.  only look at things that talk at it
+                interesting_ip = struct.pack("BBBB", 192, 168, 212, 109)
+                if (tcp.dport == 179 or tcp.sport == 179) and len(tcp.data) and ip.dst == interesting_ip:
                     try:
                         for bgp in bgp_parse(tcp.data, i):
                             #parse bgp here
                             #process(bgp)
-                                if bgp.type == dpkt.bgp.UPDATE:
-                                    print ""
-                                    print bgp.update.withdrawn
-                                    print bgp.update.announced
-                                    print ""
-                                    if len(bgp.update.withdrawn):
-                                        withdrawn_count += 1
+                            if bgp.type == dpkt.bgp.OPEN:
+                                AS = get_AS(bgp.open)
+                                ip_list[ip4_to_str(ip.src)] = AS
+                            elif bgp.type == dpkt.bgp.UPDATE:
+                                process_update(graph, bgp.update, ip_list[ip4_to_str(ip.src)])
+                                pass
+                                
                     except:
                         #print "fail on packet", i, ip4_to_str(ip.src), ip4_to_str(ip.dst)
                         bad_bgp += 1
                         raise
-            #print withdrawn_count
+
     except dpkt.dpkt.NeedData:
         continue
